456c42e1ee236a5361b2406fe456ab7c2fabb79e
shie8x <xkuiw5@outlook.com>
2024-06-21 11:40:50 +0800

    Modify condition.go

--- condition.go
+++ condition.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Approach almost last interview source education.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
