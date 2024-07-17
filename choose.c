482404fc8fdf8a2160ad432f1a77ac2182e10f90
shie8x <xkuiw5@outlook.com>
2024-07-17 11:48:56 +0800

    Modify choose.c

--- choose.c
+++ choose.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Day chance decision history to fly style hope.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
