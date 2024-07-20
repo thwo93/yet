ac6e8c3aea94597bdee973cea6c8be8748eefa06
shie8x <xkuiw5@outlook.com>
2024-07-20 13:56:55 +0800

    Modify choose.c

--- choose.c
+++ choose.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Next thought product adult whatever final.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
