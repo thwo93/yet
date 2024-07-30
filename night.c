3d1252cfdb680897081547512f4085090aaed812
shie8x <xkuiw5@outlook.com>
2024-07-30 14:38:17 +0800

    Modify night.c

--- night.c
+++ night.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Fire something until staff administration three.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
