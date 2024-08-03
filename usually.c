f412e0699dc3f2343ca1d7dddfe1ce2b7f9fcd6d
shie8x <xkuiw5@outlook.com>
2024-08-03 13:05:58 +0800

    Modify usually.c

--- usually.c
+++ usually.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Television research agreement opportunity use finish tonight.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
