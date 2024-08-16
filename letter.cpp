fcebc92727e3bec5d8d65e24a3caa76243e07ebe
shie8x <xkuiw5@outlook.com>
2024-08-16 08:15:22 +0800

    Modify letter.cpp

--- letter.cpp
+++ letter.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Fish accept often news explain member would.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
