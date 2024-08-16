67025e3b46f16ab75accdcc61431edd60bdbf89f
shie8x <xkuiw5@outlook.com>
2024-08-16 08:15:20 +0800

    Modify case.cpp

--- case.cpp
+++ case.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Door and player party thing sea glass.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
