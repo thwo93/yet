e2b28a16cfb8d0834f2ed3f854115346b51e53b9
shie8x <xkuiw5@outlook.com>
2024-08-21 19:53:34 +0800

    Modify ball.cpp

--- ball.cpp
+++ ball.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# American series structure particular near high.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
