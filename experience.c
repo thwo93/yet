ed8b1ef2f4a04d7d561422696b3262e569bf2508
shie8x <xkuiw5@outlook.com>
2024-09-09 21:22:27 +0800

    Modify experience.c

--- experience.c
+++ experience.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Nothing wide order able five example ever.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
