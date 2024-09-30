e695a9c1fddbdcb49866d60e6072be47a5d13c84
shie8x <xkuiw5@outlook.com>
2024-09-30 13:45:21 +0800

    Modify for.cpp

--- for.cpp
+++ for.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Western determine case nice.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
