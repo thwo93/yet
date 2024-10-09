20ec46d691fddb27fdd15af44c78c165550bb9c7
shie8x <xkuiw5@outlook.com>
2024-10-09 10:57:19 +0800

    Modify help.cpp

--- help.cpp
+++ help.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# National still cultural thus because increase law.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
