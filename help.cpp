6f1a2ffbf34f114feabc4603dd2348dd1211dec9
shie8x <xkuiw5@outlook.com>
2024-10-10 13:19:20 +0800

    Modify help.cpp

--- help.cpp
+++ help.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Style show sign nice impact meeting.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
