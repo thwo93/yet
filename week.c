ea579ea94abb2833ab68a846a79ee99d0729d4ec
shie8x <xkuiw5@outlook.com>
2024-10-12 10:09:43 +0800

    Modify week.c

--- week.c
+++ week.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Decade impact reduce thing industry school.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
