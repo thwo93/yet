9c3a195a8d29d09382ec6354be2d670507763e67
shie8x <xkuiw5@outlook.com>
2024-10-15 11:03:07 +0800

    Modify character.cpp

--- character.cpp
+++ character.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Single small name.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
