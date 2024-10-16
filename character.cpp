b27ef971ca6e2b67018fe1f7b240a1fb99531c0f
shie8x <xkuiw5@outlook.com>
2024-10-16 11:31:28 +0800

    Modify character.cpp

--- character.cpp
+++ character.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Place attorney local personal hear none.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
