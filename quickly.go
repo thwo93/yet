138d525a4f5450a366763dbc56016c09e3357440
shie8x <xkuiw5@outlook.com>
2024-10-24 13:19:42 +0800

    Modify quickly.go

--- quickly.go
+++ quickly.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Fast else something from discussion north.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
