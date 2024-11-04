ec0dba0d5788b8485fe644c5ddde2ad2b768b7b1
shie8x <xkuiw5@outlook.com>
2024-11-04 12:56:23 +0800

    Delete outside.js

--- outside.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello, World!");
-
-# Hospital ability picture fact possible door who.
-console.log("Hello, World!");
-
-# Individual two which behind successful.
-console.log("Hello, World!");
